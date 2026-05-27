f8c107d1eafa85c9bb73fb3afefc9dc8c4d07d25
chandar prakash <chandarprakash7122@outlook.com>
2026-05-27 12:18:39 +0530

    in program file change error

--- Program.cs
+++ Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddCors(options =>
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
-            .AllowCredentials()
+            .AllowCredentials();
             //.AllowAnyOrigin();
         });
 });
